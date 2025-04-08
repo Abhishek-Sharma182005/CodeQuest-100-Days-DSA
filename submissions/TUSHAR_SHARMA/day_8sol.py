cf15d1f2014dca58218c41a5d445c561ac570671
Tusharsharma1827 <tusharsharma27p@gmail.com>
2025-04-08 09:29:46 +0530

    sol_8

---
+++ submissions/TUSHAR_SHARMA/day_8sol.py
@@ -0,0 +1,12 @@
+#day 8 sol
+#countdown using a for loop (runs backwaeds)
+print("countdown using for loop:")
+for i in range(10,0,-1):
+    print(i)
+#countdown using while loop
+print("countdown using while loop")
+i=10
+while i>0:
+    print(i)
+    i-=1
+    
